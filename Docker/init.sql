64dc4e09f760cbdd6f59131be360cc33b47bbb8a
sh1vero <sh1veeer@gmail.com>
2022-05-24 22:54:43 +0200

    added script to grant read/write for admin_account

--- Docker/init.sql
+++ Docker/init.sql
@@ -5,3 +5,6 @@ GRANT ALL PRIVILEGES ON DATABASE myApp_dev TO admin_account;
 
 CREATE DATABASE myApp_test;
 GRANT ALL PRIVILEGES ON DATABASE myApp_test TO admin_account;
+
+GRANT pg_read_all_data TO admin_account;
+GRANT pg_write_all_data TO admin_account;
